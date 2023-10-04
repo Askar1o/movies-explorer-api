cb6995705b40eb7853f3874f04065a7b0825b81d
Askar1o <Gilmanov.Ask@yandex.ru>
2023-10-04 12:37:56 +0500

    поменял порт

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 const rateLimit = require('express-rate-limit');
 
 const SECRET = process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'dev';
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 const MONGO = process.env.MONGO_DB || 'mongodb://0.0.0.0:27017/bitfilmsdb';
 const MONGO_OPTIONS = {
   useNewUrlParser: true,
